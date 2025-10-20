9067fe9390eeaffd1a94ee0ef9302399b946ce2a
Rahaf Mohammed <rahaf786@yahoo.com>
2025-10-19 20:28:23 -0700

    commit ladder.py

---
+++ ladder.py
@@ -0,0 +1,12 @@
+def my_steps(n: int) -> int:
+    if not isinstance(n, int):
+        raise ValueError("n must be an integer")
+    if n < 1 or n > 25:
+        raise ValueError("n out of bounds (1 ≤ n ≤ 25)")
+
+    a, b = 1, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+#testing git commit
